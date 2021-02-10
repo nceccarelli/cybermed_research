fc06c897d1de2cefe2b2effe54afa2acc4bcc06a
Nicholas Ceccarelli <njceccar@buffalo.edu>
2021-02-09 23:49:22 -0500

    init from respiratory_sound repo

---
+++ scripts/keras_resnet_crackling.py
@@ -0,0 +1,187 @@
+import tensorflow as tf
+import csv
+import numpy as np
+import os
+import imageio
+from keras.utils import to_categorical
+from keras import backend as K
+
+# Folder to read in the data from
+    # Folder should contain:
+        # Training and testing folders with spectrograms
+        # 4 CSVs with training and testing data for both crackling and wheezing
+data_folder = "../database/pre_processed_data_keras_augmented/"
+lung_problem_detection = "crackling"
+
+# Get the number of samples
+def get_spectro_counts():
+    # Get number of training spectrograms
+    _, _, files = next(os.walk(data_folder + "training"))
+    training_png_count = 0
+    for file in files:
+        if "aug" not in file:
+            training_png_count += 1
+
+    # Get number of testing spectrograms
+    _, _, files = next(os.walk(data_folder + "testing"))
+    testing_png_count = 0
+    for file in files:
+        if "aug" not in file:
+            testing_png_count += 1
+
+    return training_png_count + testing_png_count
+
+# Read in the data
+def read_data():
+    x_train = []
+    x_test = []
+    y_train = []
+    y_test = []
+
+    repeats = []
+
+    # Get the total number of files
+    tot_files = get_spectro_counts()
+    print("Loading data. Files to process: ", str(tot_files))
+
+    # Get x_test
+    i = 5
+    while i <= tot_files:
+        im = imageio.imread(data_folder + "testing/sample_" + str(i) + ".png")
+        x_test.append(im[:,:,0:3])
+
+        # Check for augmented image
+        if (os.path.exists(data_folder + "testing/sample_" + str(i) + "_aug.png")):
+            # Augmented image exists
+            repeats.append(i)
+
+            # Load and saveimage
+            im = imageio.imread(data_folder + "testing/sample_" + str(i) + "_aug.png")
+            x_test.append(im[:224,:224,0:3])
+
+        i += 5
+    x_test = np.array(x_test)
+
+    # Get x_train
+    i = 1
+    while i <= tot_files:
+        im = imageio.imread(data_folder + "training/sample_" + str(i) + ".png")
+        x_train.append(im[:,:,0:3])
+
+        # Check for augmented image
+        if (os.path.exists(data_folder + "training/sample_" + str(i) + "_aug.png")):
+            # Augmented image exists
+            repeats.append(i)
+
+            # Load and saveimage
+            im = imageio.imread(data_folder + "training/sample_" + str(i) + "_aug.png")
+            
+            # TODO: This is a stopgap, must rescale, not resize
+            x_train.append(im[:224,:224,0:3])
+
+        if ((i+1)%5==0): i += 2
+        else: i += 1
+    x_train = np.array(x_train)
+
+    # Get y_train
+    with open(data_folder + "training_" + lung_problem_detection + "_truth.csv", 'r') as tcsv:
+        for line in csv.reader(tcsv):
+            i = 1
+            for elem in line:
+                y_train.append(int(elem))
+                if (i in repeats): y_train.append(int(elem))
+
+                if ((i+1)%5==0): i += 2
+                else: i += 1
+    y_train = np.asarray(y_train)
+    
+    # Get y_test
+    with open(data_folder + "testing_" + lung_problem_detection + "_truth.csv", 'r') as tcsv:
+        for line in csv.reader(tcsv):
+            i = 5
+            for elem in line:
+                y_test.append(int(elem))
+
+                if (i in repeats): y_test.append(int(elem))
+                i += 5
+                
+    y_test = np.asarray(y_test)
+
+    print("Fininshed loading data")
+
+    print("X train", x_train.shape)
+    return (x_train, y_train), (x_test, y_test)
+
+# Calculate recall
+def recall(y_true, y_pred):
+    """Recall metric.
+
+    Only computes a batch-wise average of recall.
+
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+# Calculate precision
+def precision(y_true, y_pred):
+    """Precision metric.
+
+    Only computes a batch-wise average of precision.
+
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+# Calculate F-1 score
+def f1(y_true, y_pred):
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    return 2*((prec*rec)/(prec+rec+K.epsilon()))
+
+# Create ResNet pretrained on ImageNet
+model = tf.keras.applications.ResNet50(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000
+)
+
+# I may be able to add a keras LSTM layer here
+
+# Configure learning rate for optimizer
+opt = tf.keras.optimizers.Adam(0.01)
+# optimizer.learning_rate.assign(0.01)
+
+# Compile the model
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy', f1, precision, recall]
+)
+
+# Read in the data
+(x_train, y_train), (x_test, y_test) = read_data()
+
+# Run model
+model.fit(
+    x_train, y_train, 
+    batch_size = 32, 
+    epochs = 10, 
+    verbose = 1 
+)
+
+# Evaluate model
+results = model.evaluate(
+    x_test, y_test, 
+    batch_size=128
+)
---
+++ scripts/pre_process_keras_butterworth.py
@@ -0,0 +1,237 @@
+# File processing
+import os
+import fnmatch
+import csv
+
+# Math
+import numpy as np
+
+# Plotting
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# Audio processing
+import librosa
+import librosa.display
+from pydub import AudioSegment
+
+# Image processing
+from PIL import Image
+
+# For set_size()
+from matplotlib.image import imread
+from tempfile import NamedTemporaryFile
+
+# GitHub repo from Google
+# import spec_augment_tensorflow
+
+from scipy.signal import butter, sosfilt
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
+    return sos
+
+def get_size(fig, dpi=100):
+    with NamedTemporaryFile(suffix='.png') as f:
+        fig.savefig(f.name, bbox_inches='tight', dpi=dpi)
+        height, width, _channels = imread(f.name).shape
+        return width / dpi, height / dpi
+
+def set_size(fig, size, dpi=100, eps=1e-2, give_up=2, min_size_px=10):
+    target_width, target_height = size
+    set_width, set_height = target_width, target_height # reasonable starting point
+    deltas = [] # how far we have
+    while True:
+        fig.set_size_inches([set_width, set_height])
+        actual_width, actual_height = get_size(fig, dpi=dpi)
+        set_width *= target_width / actual_width
+        set_height *= target_height / actual_height
+        deltas.append(abs(actual_width - target_width) + abs(actual_height - target_height))
+        if deltas[-1] < eps:
+            return True
+        if len(deltas) > give_up and sorted(deltas[-give_up:]) == deltas[-give_up:]:
+            return False
+        if set_width * dpi < min_size_px or set_height * dpi < min_size_px:
+            return False
+
+# Directory storing input audio files
+input_dir = "../database/audio_and_txt_files/"
+
+# Directory where you want the spectrograms stored
+output_dir = "../database/preprocessed_data_butterworth/"
+
+max_time = 3.5
+min_time = 0.5
+keep_time = 2.0
+half_keep_time = keep_time/2.0
+freq_min = 0
+freq_max = 4000
+
+num_pixels = 80388
+skipped_count = 0
+
+sample_count = 0 # keeps track of current sample
+placement_flag = "training" # keeps track of training vs testing data
+
+training_crackling_truth = [] # store for training data, later stored to CSV
+training_wheezing_truth = []
+testing_crackling_truth = [] #  store for testing data, later stored to CSV
+testing_wheezing_truth = []
+
+patient = 1 # keeps track of current patient
+
+for file in os.listdir(input_dir): #loop through all files in input directory
+    if fnmatch.fnmatch(file, "*.wav"): #if its a WAV
+        root_file = file[0:len(file)-3] #get file name minus "WAV"
+        txt_file = file[0:len(file)-3] + "txt" # add "wav" for audio, "txt" for info
+        print(txt_file)
+        with open(input_dir + txt_file) as tsv: # open associated TXT file
+
+            for line in csv.reader(tsv, delimiter="\t"): #iterate through each line of the text file
+
+                # read start and end time from file
+                recorded_start_time = float(line[0])
+                recorded_end_time = float(line[1])
+
+                # Do not process respirations longer than 4 seconds or less than 1 second in duration
+                if (recorded_end_time - recorded_start_time > max_time or recorded_end_time - recorded_start_time < min_time): continue
+
+                # Mid-point of recording
+                middle_start_time = recorded_start_time + (recorded_end_time - recorded_start_time)
+                
+                # Standardize data to 2.8 seconds in length as best as possible
+                start_time = (middle_start_time - keep_time/2) * 1000
+                end_time = (middle_start_time + keep_time/2) * 1000
+
+                # make sure start time is not negative
+                if start_time < 0: 
+                    start_time = 0
+                    if (recorded_end_time >= keep_time):
+                        end_time = keep_time*1000
+
+                # Make sure end time is not over the length of the respiration
+                elif end_time / 1000 > recorded_end_time: 
+                    start_time = (recorded_end_time - keep_time) * 1000
+                    end_time = recorded_end_time * 1000
+                    if start_time < 0: start_time = 0
+    
+                # Skip respiration if regularization of data fails
+                if (end_time - start_time != 2000): continue
+
+                # Record next sample
+                sample_count += 1
+
+                # figure out if it's testing or training
+                if (not sample_count % 5): placement_flag = "testing" # assign every fifth to testing
+                else: placement_flag = "training" # assign four of five to training
+
+                # Update status to user
+                print("Processing sample", str(sample_count), "| From patient", patient)
+
+                # read if the respiration crackles or wheezes
+                crackles = int(line[2])
+                wheezes = int(line[3])
+
+                # get WAV and segment it
+                sep_audio = AudioSegment.from_wav(input_dir + file) #get audio file
+                sep_audio = sep_audio[start_time:end_time] #separate time
+                sep_audio.export('intermediate2.wav', format='wav')
+
+
+
+                # Load WAV and create spectrogram
+                y,sr = librosa.load("intermediate2.wav")
+                sos = butter_bandpass(120, 1800, 5000, order=12)
+                y = sosfilt(sos, y)
+                S = librosa.feature.melspectrogram(y=y, sr=sr,fmin=freq_min,fmax=freq_max)
+                S_dB = librosa.power_to_db(S, ref=np.max)
+                librosa.display.specshow(S_dB, x_axis='time',
+                                        y_axis='mel', sr=sr,
+                                        fmin=freq_min,fmax=freq_max)
+                
+                # Save spectrogram
+                plt.axis("off")
+                fig = plt.gcf()
+                fig.set_size_inches(8, 8)
+                sample_save_dir = output_dir + placement_flag + '/sample_' + str(sample_count) + '.png'
+                set_size(fig, (2.44,2.44))
+                plt.savefig(sample_save_dir, bbox_inches="tight", pad_inches = 0)
+
+                # # Open image for quality check
+                # image = Image.open(sample_save_dir)
+                # im_arr = np.asarray(image)
+
+                # # run quality check
+                # peak_pix = 0
+                # for column in im_arr:
+                #     for (r,g,b,_) in column:
+                #         r,g,b = int(r),int(g),int(b)
+                #         if b*2-40  < (r+g): peak_pix += 1
+                        
+                # # If there is too much yellow, skip. Quality check declined
+                # if (peak_pix/num_pixels) > 0.4: 
+                # # Quality check failed
+                #     os.remove(sample_save_dir)
+                #     print("Sample", str(sample_count), "skipped.")
+                #     sample_count -= 1
+                #     skipped_count += 1
+                # else:
+                # Quality check passed
+                    # Run data augmentation
+
+                    # Create spectrogram
+                    # mel_spectrogram = librosa.feature.melspectrogram(y=y,
+                    #                                     sr=sr,
+                    #                                     n_mels=256,
+                    #                                     hop_length=128,
+                    #                                     fmax=freq_max)
+                    # S_dB = librosa.power_to_db(mel_spectrogram, ref=np.max)
+                    # librosa.display.specshow(S_dB, x_axis='time',
+                    #                         y_axis='mel', sr=sr,
+                    #                         fmin=freq_min,fmax=freq_max)
+                    
+                    # # Save augmented spectrogram
+                    # plt.axis("off")
+                    # fig = plt.gcf()
+                    # fig.set_size_inches(4.5, 3)
+                    # augmented_sample_save_dir = output_dir + placement_flag + '/sample_' + str(sample_count) + '-1.png'
+                    # plt.savefig(augmented_sample_save_dir, bbox_inches="tight", pad_inches = 0)
+                    
+                    # append the ground truth to corresponding list
+                if (placement_flag == 'testing'):
+                    testing_crackling_truth.append(crackles)
+                    testing_wheezing_truth.append(wheezes)
+                else: 
+                    training_crackling_truth.append(crackles)
+                    training_wheezing_truth.append(wheezes)
+                
+                # Remove intermediate file
+                os.remove("intermediate2.wav")
+
+        #increment patient 
+        patient += 1
+
+# write truth training data to CSV
+with open(output_dir + 'training_crackling_truth.csv', 'w') as training_file:
+    wr = csv.writer(training_file)
+    wr.writerow(training_crackling_truth)
+
+with open(output_dir + 'training_wheezing_truth.csv', 'w') as training_file:
+    wr = csv.writer(training_file)
+    wr.writerow(training_wheezing_truth)
+
+# write truth testing data to CSV
+with open(output_dir + 'testing_crackling_truth.csv', 'w') as testing_file:
+    wr = csv.writer(testing_file)
+    wr.writerow(testing_crackling_truth)
+
+# write truth testing data to CSV
+with open(output_dir + 'testing_wheezing_truth.csv', 'w') as testing_file:
+    wr = csv.writer(testing_file)
+    wr.writerow(testing_wheezing_truth)
+
+
+print("Skipped count: ", str(skipped_count))
